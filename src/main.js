ba4fef4fd13d0ef2786488670d76c7a3d3454fd8
Antonio Quintanilla <65049848+AntonioQuintanilla@users.noreply.github.com>
2024-09-19 14:31:46 -0600

    CRUD basico terminado, usando Electron y MariaDB

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const { BrowserWindow } = require('electron');
+const { BrowserWindow, ipcMain, Notification } = require('electron');
+const { getConnection } = require('./database')
 
 let window
 
@@ -7,12 +8,85 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
     }
   })
   window.loadFile('src/ui/index.html');
 }
 
+// Función para insertar un producto en la base de datos
+async function createProduct(product) {
+  try {
+    const conn = await getConnection(); // Espera la conexión a la base de datos
+    product.price = parseFloat(product.price); // Convierte el precio a número
+    const result = await conn.query('INSERT INTO product SET ?', product); // Inserta el producto
+    console.log(result);
+
+    new Notification({
+      title: 'Electron MySQL',
+      body: 'New product saved successfully'
+    }).show();
+
+    product.id = result.insertId; // Asigna el ID del producto insertado al objeto
+    return product; // Devuelve el objeto del producto insertado
+    
+  //   console.log('Product inserted:', result.insertId); // ID del producto insertado
+  //   return result.insertId; // Devuelve el ID del producto insertado
+  } catch (error) {
+    console.error('Error inserting product:', error);
+    throw error; // Lanza el error para ser manejado en el renderer si es necesario
+  }
+}
+
+async function getProducts() {
+  const conn = await getConnection();
+  const result = await conn.query('SELECT * FROM product');
+  return result;
+}
+
+async function deleteProduct(id) {
+  const conn = await getConnection();
+  const result = await conn.query('DELETE FROM product WHERE id = ?', id);
+  return result;
+}
+
+async function getProductById(id) {
+    const conn = await getConnection();
+    const result = await conn.query('SELECT * FROM product WHERE id = ?', id);
+    return result[0];
+}
+
+async function updateProduct(product) {
+    const conn = await getConnection();
+    const result = await conn.query('UPDATE product SET ? WHERE id = ?', [product, product.id]);
+    return result;
+}
+
+
+// Manejar la solicitud desde el proceso renderer para insertar un producto
+ipcMain.handle('create-product', async (event, product) => {
+  return await createProduct(product);
+});
+
+ipcMain.handle('get-products', async (event) => {
+  return await getProducts();
+});
+
+ipcMain.handle('delete-product', async (event, id) => {
+  return await deleteProduct(id);
+});
+
+ipcMain.handle('get-product-by-id', async (event, id) => {
+  return await getProductById(id);
+});
+
+ipcMain.handle('update-product', async (event, product) => {
+  return await updateProduct(product);
+});
+
+
 module.exports = {
-  createWindow
+  createWindow,
 }
---
+++ src/ui/app.js
@@ -0,0 +1,94 @@
+// ipcRenderer: Módulo que permite enviar eventos desde el proceso de renderizado al proceso principal
+const { ipcRenderer } = require('electron');
+
+// Lógica de la interfaz de usuario
+const productForm = document.getElementById('productForm');
+const productName = document.getElementById('name');
+const productPrice = document.getElementById('price');
+const productDescription = document.getElementById('description');
+const productsList = document.getElementById('products');
+
+let products = [];
+let editingStatus = false;
+let editProductId = '';
+
+productForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const newProduct = {
+        name: productName.value,
+        price: productPrice.value,
+        description: productDescription.value
+    }
+
+    if(!editingStatus) {
+        const result = await ipcRenderer.invoke('create-product', newProduct);
+        console.log(result);
+    } else {
+        await ipcRenderer.invoke('update-product', { ...newProduct, id: editProductId });
+        editingStatus = false;
+        editProductId = '';
+    }
+
+    productForm.reset(); // Limpiar el formulario
+    productName.focus(); // Enfocar el campo de nombre
+
+    await getProducts(); // Actualizar la lista de productos
+})
+
+const deleteProduct = async (id) => {
+    if(confirm('Are you sure you want to delete this product?')) {
+        await ipcRenderer.invoke('delete-product', id);
+        await getProducts();
+    }
+}
+
+
+const updateProduct = async (id) => {
+    const product = await ipcRenderer.invoke('get-product-by-id', id);
+    productName.value = product.name;
+    productPrice.value = product.price;
+    productDescription.value = product.description;
+
+    editingStatus = true;
+    editProductId = product.id;
+}
+
+const renderProducts = (products) => {
+    productsList.innerHTML = '';
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.classList.add('card', 'card-body', 'my-2', 'animate__animated', 'animate__fadeInLeft');
+
+        productCard.innerHTML += `
+                <h4>${product.name}</h4>
+                <p>${product.description}</p>
+                <h3>${product.price}</h3>
+                <p>
+                  <button class="btn btn-danger delete-btn">DELETE</button>  
+                  <button class="btn btn-secondary update-btn">EDIT</button>
+                </p>
+        `;
+
+        productCard.querySelector('.delete-btn').addEventListener('click', () => {
+            deleteProduct(product.id);
+        });
+        productCard.querySelector('.update-btn').addEventListener('click', () => {
+            updateProduct(product.id);
+        });
+        // Agregar la tarjeta del producto al contenedor de productos
+        productsList.appendChild(productCard);
+    });
+}
+
+const getProducts = async () => {
+    products = await ipcRenderer.invoke('get-products');
+    renderProducts(products);
+}
+
+async function init() {
+    await getProducts();
+    console.log('Products:', products);
+}
+
+init();
